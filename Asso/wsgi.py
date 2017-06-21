8107540a58ec4d812e711d7b79a18d47d198f106
zartch <zartch@gmail.com>
2017-06-21 20:57:50 +0200

    Dolor de muelas

--- Asso/wsgi.py
+++ Asso/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "asso.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Asso.settings")
 
 application = get_wsgi_application()
